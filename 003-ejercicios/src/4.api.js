60b9df18195331bc939e03744f8b8550b6ed1121
Juan D <tikinni97@gmail.com>
2020-06-29 19:49:11 -0300

    primer commit

--- 003-ejercicios/src/4.api.js
+++ 003-ejercicios/src/4.api.js
@@ -2,3 +2,33 @@ import fetch from 'node-fetch';
 
 // fetch('..... aquí va la url')
 // recordar que fetch devuelve una promise, entonces puede hacer fetch(....).then(resultado => {.... })
+export const api = id => {
+    fetch('https://jsonplaceholder.typicode.com/users').then(response => {
+      return response.json();
+    }).then(json => {
+      let res = json.find(element => element.id === id);
+      console.log(res.name);
+      console.log(res.address.city);
+    })
+    .catch(err => {
+        console.log("ocurrio algo");
+    });
+  };
+
+  export const getRemoteData= async (id) => {
+    let datos= await (await fetch('https://jsonplaceholder.typicode.com/users')).json();
+    datos= datos.find(element => element.id === id);
+    console.log(datos.name, datos.address.city);
+  }
+
+  const delay = async (segundos) => {
+
+  }
+  let run = () => {
+    console.log(1);
+    delay('Terminó 1', 1000);
+    console.log(2);
+    delay('Terminó 2', 1000);
+    console.log(3);
+    delay('Terminó 3', 1000);
+  }
---
+++ 003-ejercicios/src/index.js
@@ -0,0 +1,3 @@
+import {api , getRemoteData} from './4.api';
+console.log(api(3));
+console.log(getRemoteData(3));
